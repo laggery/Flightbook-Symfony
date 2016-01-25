fdc256dfaf852f3d272685cbf58beb4d1d60f1f3
Yannick Lagger <lagger.yannick@gmail.com>
2016-01-26 00:21:03 +0100

    Form responsive + checkbox hack

---
+++ flightbook/web/js/animation.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // @hack Permet d'avoir l'affichage souhaité de la checkbox (moche)
+    $('#glider_tandem').appendTo(".form-group:last");
+    $('#glider .checkbox label').appendTo(".form-group:last");
+    $('#glider_tandem').appendTo(".form-group:last");
+    $('#glider .checkbox').hide();
+});
