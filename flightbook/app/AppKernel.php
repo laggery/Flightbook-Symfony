e95529624d0e05db940859642c288dc8ef011300
Yannick Lagger <lagger.yannick@gmail.com>
2016-03-03 00:28:07 +0100

    Install jms/i18n-routing-bundle for fix routing bug

--- flightbook/app/AppKernel.php
+++ flightbook/app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+            new AppBundle\AppBundle(),            
             new UserBundle\UserBundle(),
             new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
         );
